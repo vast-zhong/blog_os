0113f390e5323fc4780ecd3f1d6cd21f68f73b3b
vast-zhong <zhonguanghan@gmail.com>
2025-09-13 15:48:12 +0800

    feat(vga_buffer): create vga_buffer, and write something on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
 
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
@@ -14,14 +12,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)] // 不重整函数名
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    
+    vga_buffer::print_something(); 
     loop {}
 }
