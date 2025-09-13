2da10e3bed1144c992132292288bf8c68ff28109
vast-zhong <zhonguanghan@gmail.com>
2025-09-13 14:17:21 +0800

    feat(main): Successfully compile the system image using bootimage, but can't run normally in qemu

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-// src/main.rs
 
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
 
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
@@ -13,7 +14,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)] // 不重整函数名
 pub extern "C" fn _start() -> ! {
-    // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
-    // 默认命名为 `_start`
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    
     loop {}
 }
